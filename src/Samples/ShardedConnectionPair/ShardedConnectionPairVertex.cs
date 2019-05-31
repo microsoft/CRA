6b75429a35635ee5290045224453108e04968330
Badrish Chandramouli <badrishc@microsoft.com>
2019-05-30 19:13:07 -0700

    Bug fixes

--- src/Samples/ShardedConnectionPair/ShardedConnectionPairVertex.cs
+++ src/Samples/ShardedConnectionPair/ShardedConnectionPairVertex.cs
@@ -11,7 +11,7 @@ namespace ShardedConnectionPair
 
         public override void Initialize(int shardId, ShardingInfo shardingInfo, object vertexParameter)
         {
-            Console.WriteLine("Sharded vertex name", GetVertexName());
+            Console.WriteLine("Sharded vertex name: {0}", GetVertexName());
 
             AddAsyncInputEndpoint("input", new MyAsyncInput(this));
             AddAsyncOutputEndpoint("output", new MyAsyncOutput(this));
