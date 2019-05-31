3be545d7172169aed7192f81fd28de9f8e8e23db
Ibrahim Sabek <sabek@cs.umn.edu>
2019-05-31 16:39:37 -0500

    Rerverting back to .Wait() calls for some Azure blobs and tables calls

--- src/CRA.ClientLibrary/AzureProvider/AzureShardedVertexInfoProvider.cs
+++ src/CRA.ClientLibrary/AzureProvider/AzureShardedVertexInfoProvider.cs
@@ -2,6 +2,7 @@
 {
     using CRA.ClientLibrary.DataProvider;
     using Microsoft.WindowsAzure.Storage.Table;
+    using System;
     using System.Collections.Generic;
     using System.Linq;
     using System.Threading.Tasks;
@@ -37,15 +38,15 @@
                         "PartitionKey",
                         QueryComparisons.Equal,
                         vertexName));
-            
-            /*List<ShardedVertexInfo> vtEntries = new List<ShardedVertexInfo>();
+
+            List<ShardedVertexInfo> vtEntries = new List<ShardedVertexInfo>();
             foreach (var vt in (await cloudTable.ExecuteQueryAsync(query)))
                 vtEntries.Add(new ShardedVertexInfo(vt.VertexName, vt.EpochId, vt.AllInstances, vt.AllShards, vt.AddedShards, vt.RemovedShards, vt.ShardLocator));
 
-            return vtEntries;*/
+            return vtEntries;
 
-            return (await cloudTable.ExecuteQueryAsync(query))
-                .Select(vt => (ShardedVertexInfo)vt);
+            /*return (await cloudTable.ExecuteQueryAsync(query))
+                .Select(vt => (ShardedVertexInfo)vt);*/
         }
 
         public async Task<ShardedVertexInfo> GetLatestEntryForVertex(string vertexName)
--- src/CRA.ClientLibrary/Main/ShardedCRAClientLibrary.cs
+++ src/CRA.ClientLibrary/Main/ShardedCRAClientLibrary.cs
@@ -42,9 +42,8 @@ namespace CRA.ClientLibrary
                 isActive: true,
                 isSharded: false);
 
-            //_vertexManager.VertexInfoProvider.InsertOrReplace(newRow).Wait();
-            await _vertexManager.VertexInfoProvider.InsertOrReplace(newRow);
-
+             _vertexManager.VertexInfoProvider.InsertOrReplace(newRow).Wait();
+             //await _vertexManager.VertexInfoProvider.InsertOrReplace(newRow);
 
             return CRAErrorCode.Success;
         }
@@ -91,12 +90,11 @@ namespace CRA.ClientLibrary
                 }
             }
 
-
-            //_shardedVertexTableManager.DeleteShardedVertex(vertexName).Wait();
-            await _shardedVertexTableManager.DeleteShardedVertex(vertexName);
-            //_shardedVertexTableManager.RegisterShardedVertex(vertexName, allInstances, allShards, addedShards, removedShards, shardLocator).Wait();
-            await _shardedVertexTableManager.RegisterShardedVertex(vertexName, allInstances, allShards, addedShards, removedShards, shardLocator);
-
+            _shardedVertexTableManager.DeleteShardedVertex(vertexName).Wait();
+            //await _shardedVertexTableManager.DeleteShardedVertex(vertexName);
+            _shardedVertexTableManager.RegisterShardedVertex(vertexName, allInstances, allShards, addedShards, removedShards, shardLocator).Wait();
+            //await _shardedVertexTableManager.RegisterShardedVertex(vertexName, allInstances, allShards, addedShards, removedShards, shardLocator);
+            
             CRAErrorCode[] results = Task.WhenAll(tasks).Result;
 
             // Check for the status of instantiated vertices
